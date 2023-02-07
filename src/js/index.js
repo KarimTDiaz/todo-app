0f746eaa9972f3ee86d6953228c213d7f4ab58ed
AlejandroJimenezMurciano <karimtilouni94@gmail.com>
2023-02-07 14:57:59 +0100

    Todo App

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,39 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { createTasks } from './tasks.js';
-import { filterTask } from './filter.js';
+import {
+  createTaskObj,
+  taskItemsElement,
+  removeTask,
+  completedTask,
+  filterTasks
+} from './tasks.js';
 
 const inputTaskElement = document.getElementById('create-todo');
 const activeBtnElement = document.getElementById('active');
 const completedBtnElement = document.getElementById('completed');
 const clearBtnElement = document.getElementById('clear');
 
+const filterElements = document.getElementById('task-filters');
+
 inputTaskElement.addEventListener('submit', ev => {
   ev.preventDefault();
-  createTasks(ev.target.todo.value);
+  createTaskObj(ev.target.todo.value);
 
   ev.target.todo.value = '';
 });
 
-/* taskItemsElement.addEventListener('change', ev => {
-  filterTask(ev.target);
-}); */
+taskItemsElement.addEventListener('click', ev => {
+  if (ev.target.classList.contains('cross')) {
+    removeTask(ev.target.previousElementSibling.id);
+    return;
+  }
+  if (ev.target.tagName === 'LABEL') {
+    completedTask(ev.target.htmlFor);
+  }
+});
 
-activeBtnElement.addEventListener('click', ev => {
-  filterTask();
+filterElements.addEventListener('click', ev => {
+  if (ev.target.classList.contains('button--active')) {
+    filterTasks(ev.target);
+  }
 });
--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,36 +1,73 @@
 const taskItemsElement = document.getElementById('task-items');
 const getTimeStamp = () => Date.now();
+let tasksArray = [];
 
-const tasksArray = [];
-
-const createTasks = task => {
-  const timeStamp = getTimeStamp();
-  const taskItemElement = document.createElement('div');
-  const taskLabel = document.createElement('label');
-  const taskInput = document.createElement('input');
-  const crossElement = document.createElement('img');
-  taskItemElement.classList.add('tasks__item');
-  taskLabel.classList.add('label');
-  taskLabel.textContent = task;
-  taskLabel.htmlFor = timeStamp;
-  taskInput.id = timeStamp;
-  taskInput.type = 'checkbox';
-  taskInput.classList.add('input-checked');
-  crossElement.src = 'assets/images/icon-cross.svg';
-  crossElement.classList.add('cross');
-  crossElement.addEventListener(
-    'click',
-    ev => {
-      taskItemElement.remove();
-    },
-    { once: true }
-  );
-  taskItemElement.append(taskLabel, taskInput, crossElement);
-  taskItemsElement.append(taskItemElement);
-
-  let tasks = [taskInput];
-  tasksArray.push(tasks);
-  return tasks;
+const taskPrint = fragment => {
+  taskItemsElement.innerHTML = '';
+  taskItemsElement.append(fragment);
 };
 
-export { createTasks, filterTasks, createTaskObj };
+const createTasks = tasksArray => {
+  const fragment = document.createDocumentFragment();
+  tasksArray.forEach(task => {
+    const taskItemElement = document.createElement('div');
+    const taskLabel = document.createElement('label');
+    const taskInput = document.createElement('input');
+    const crossElement = document.createElement('img');
+    taskItemElement.classList.add('tasks__item');
+    taskLabel.classList.add('label');
+    taskLabel.textContent = task.task;
+    taskLabel.htmlFor = task.id;
+    taskInput.id = task.id;
+    taskInput.checked = task.checked;
+    taskInput.type = 'checkbox';
+    taskInput.classList.add('input-checked');
+    crossElement.src = 'assets/images/icon-cross.svg';
+    crossElement.classList.add('cross');
+    taskItemElement.append(taskLabel, taskInput, crossElement);
+    fragment.append(taskItemElement);
+  });
+  taskPrint(fragment);
+};
+
+const createTaskObj = task => {
+  const timeStamp = getTimeStamp();
+  const newObj = {};
+  newObj.task = task;
+  newObj.id = timeStamp;
+  newObj.checked = false;
+  tasksArray.push(newObj);
+  createTasks(tasksArray);
+};
+
+const removeTask = id => {
+  let arrayFiltered = tasksArray.filter(task => Number(id) !== task.id);
+  tasksArray = arrayFiltered;
+  createTasks(tasksArray);
+};
+
+const completedTask = id => {
+  let arrayFiltered = tasksArray.map(task => {
+    console.log(task.id);
+    if (Number(id) === task.id) {
+      task.checked = !task.checked;
+    }
+    return task;
+  });
+  tasksArray = arrayFiltered;
+  createTasks(tasksArray);
+};
+
+const filterTasks = ev => {
+  console.log(ev);
+};
+
+export {
+  createTasks,
+  createTaskObj,
+  taskItemsElement,
+  tasksArray,
+  removeTask,
+  completedTask,
+  filterTasks
+};
